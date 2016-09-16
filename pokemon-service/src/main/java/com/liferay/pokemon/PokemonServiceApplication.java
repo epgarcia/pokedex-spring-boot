0c7bd97ab0c945f0beb29d2edc73e9672f79fe9a
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-09-16 11:01:32 +0200

    Creates pokemon-service spring boot application

---
+++ pokemon-service/src/main/java/com/liferay/pokemon/PokemonServiceApplication.java
@@ -0,0 +1,12 @@
+package com.liferay.pokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonServiceApplication.class, args);
+	}
+}
