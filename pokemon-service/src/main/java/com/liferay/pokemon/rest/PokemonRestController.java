ee6c3b484bea9687b022dab89f65fe7c581811ab
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-09-16 12:30:10 +0200

    Pokemon Service - Adds example for RequestParam

--- pokemon-service/src/main/java/com/liferay/pokemon/rest/PokemonRestController.java
+++ pokemon-service/src/main/java/com/liferay/pokemon/rest/PokemonRestController.java
@@ -2,14 +2,17 @@ package com.liferay.pokemon.rest;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PokemonRestController {
 
 	@RequestMapping(value = "/pokemon/get", method = RequestMethod.GET)
-	public String getPokemon() {
-		return "Gotta Catch 'Em All";
+	public String getPokemon(
+		@RequestParam(value = "name", defaultValue = "") String name) {
+
+		return "Gotta Catch 'Em All " + name;
 	}
 
 }
