bec5e2b31e4a4d9f0e8f324b6eea8e0d4603fa06
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-09-16 09:05:48 +0200

    Pokemon Web - Creates Pokemon model class

---
+++ pokemon-web/src/main/java/com/liferay/pokemon/model/Pokemon.java
@@ -0,0 +1,67 @@
+package com.liferay.pokemon.model;
+
+public class Pokemon {
+
+	public Pokemon() {
+	}
+
+	public Pokemon(String name, String type) {
+		this(0, name, type, "");
+	}
+
+	public Pokemon(
+		long pokemonId, String name, String type, String smallImageURL) {
+
+		this.pokemonId = pokemonId;
+		this.name = name;
+		this.type = type;
+		this.smallImageURL = smallImageURL;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Long getPokemonId() {
+		return pokemonId;
+	}
+
+	public void setPokemonId(Long pokemonId) {
+		this.pokemonId = pokemonId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getSmallImageURL() {
+		return smallImageURL;
+	}
+
+	public void setSmallImageURL(String smallImageURL) {
+		this.smallImageURL = smallImageURL;
+	}
+
+	private String id;
+	private long pokemonId;
+	private String name;
+	private String type;
+	private String smallImageURL;
+
+}
