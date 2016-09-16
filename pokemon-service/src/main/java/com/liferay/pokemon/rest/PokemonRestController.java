53eb52526d715291745533c0cdfac17cebd1b765
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2016-09-16 12:44:39 +0200

    Pokemon Service - Implements Rest API

--- pokemon-service/src/main/java/com/liferay/pokemon/rest/PokemonRestController.java
+++ pokemon-service/src/main/java/com/liferay/pokemon/rest/PokemonRestController.java
@@ -1,29 +1,50 @@
 package com.liferay.pokemon.rest;
 
 import com.liferay.pokemon.model.Pokemon;
+import com.liferay.pokemon.repository.PokemonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/pokemon")
 public class PokemonRestController {
 
-	@RequestMapping(value = "/pokemon/get", method = RequestMethod.GET)
-	public Pokemon getPokemon(
-		@RequestParam(value = "name", defaultValue = "") String name) {
+	@Autowired
+	private PokemonRepository repository;
 
-		Pokemon pokemon = new Pokemon(name, "");
+	@RequestMapping(value = "/add", method = RequestMethod.PUT)
+	public Pokemon addPokemon(
+		@RequestParam(value="pokemonId") Long pokemonId,
+		@RequestParam(value="name") String name,
+		@RequestParam(value="type") String type,
+		@RequestParam(value="smallImageURL", defaultValue = "") String smallImageURL) {
 
-		return pokemon;
+		Pokemon pokemon = new Pokemon(pokemonId, name, type, smallImageURL);
+
+		return repository.save(pokemon);
 	}
 
-	@RequestMapping(value = "/pokemon/get/{name}", method = RequestMethod.GET)
-	public Pokemon getPokemonByName(@PathVariable(value = "name") String name) {
-		Pokemon pokemon = new Pokemon(name, "");
+	@RequestMapping(value = "/get/{pokemonId}", method = RequestMethod.GET)
+	public String getPokemon(@PathVariable Long pokemonId) {
+		Pokemon pokemon = repository.findByPokemonId(pokemonId);
 
-		return pokemon;
+		return pokemon.toString();
+	}
+
+	@RequestMapping(value = "/get/all", method = RequestMethod.GET)
+	public List<Pokemon> getPokemons() {
+		return repository.findByAssetType("pokemon");
+	}
+
+	@RequestMapping(value = "/get/type/{type}", method = RequestMethod.GET)
+	public List<Pokemon> getPokemonByType(@PathVariable String type) {
+		return repository.findByType(type);
 	}
 
 }
